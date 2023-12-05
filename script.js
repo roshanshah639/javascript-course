1dc5c5961b426715c441eb9ac00e49a15043e69d
Roshan Shah <85742866+roshanshah639@users.noreply.github.com>
2023-12-05 01:46:56 +0000

    conversion of data types

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log("Hello, World!");
+// variables and constants
+const accountId = 1445533;
+let accountEmail = "roshan.shah@gmail.com";
+var accountPassword = "123456";
+accountCity = "Kathmandu";
+let accountState = "Bagmati";
+
+
+// accountId = 1445534; // Not allowed
+
+accountEmail = "roshan.shah1@gmail.com";
+// console.table({ accountId, accountEmail, accountPassword, accountCity });
+
+// alert("Hello"); // we are using node js
+
+// console.log("Hello");
+
+let name = "Roshan";
+let age = 25;
+let isLoggedIn = true;
+
+// Object 
+// console.log(typeof name);
+
+let score = undefined;
+// console.log( score);
+// console.log(typeof (score));
+
+let valueInNumber = Number("33abc");
+// console.log(valueInNumber);
+
+isLoggedIn = false;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+// let strNumber = String(someNumber);
+// console.log(typeof strNumber);
+
