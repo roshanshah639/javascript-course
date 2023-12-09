62fde396bbc088c1c8034da1217c7f8a17128341
Roshan Shah <85742866+roshanshah639@users.noreply.github.com>
2023-12-09 02:36:51 +0000

    loops

---
+++ iterations.js
@@ -0,0 +1,56 @@
+// for
+
+for (let i = 0; i <= 10; i++) {
+  const element = i;
+  if (element === 5) {
+    // console.log("5 is the best number");
+  }
+  //    console.log(element);
+}
+// console.log(element);
+
+for (let i = 1; i <= 10; i++) {
+  // console.log(`Table Of ${i}`);
+  for (let j = 1; j <= 10; j++) {
+    // console.log(`Inner loop ${j}`);
+    // console.log(i + '*' + j + "=" + i*j);
+  }
+}
+// let myArray = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+// console.log(myArray.length);
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     // console.log(element);
+// }
+
+// break and continue
+for (let i = 1; i <= 20; i++) {
+  if (i === 5) {
+    // console.log(`Detected 5`);
+    // break;
+    continue;
+  }
+  // console.log(`Value of i is ${i}`);
+}
+
+// while loop
+// let i = 0;
+// while (i <= 10) {
+//   console.log(`Value of i is ${i}`);
+//   i = i+2;
+// }
+
+// let myArray = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+// let arr = 0;
+// while (arr < myArray.length) {
+//   // console.log(myArray[arr]);
+//   // arr++;
+// }
+
+//do while loop
+// let score = 11;
+// do {
+//   console.log(`Value of score is ${score}`);
+//   score++;
+// } while (score <= 10);
+
